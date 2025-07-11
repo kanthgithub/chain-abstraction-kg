29b1344501287807c6a9d6ea598d3035f5299ade
kanthgithub <kanth.explorer@gmail.com>
2025-07-11 20:56:33 +0530

    fix: remove obselete files

--- ingestor.py
+++ ingestor.py
@@ -1,36 +1,15 @@
-from dto import UserIntentDTO, BidDTO
 from graph_builder import GraphBuilder
 from neo4j_driver import Neo4jClient
+from dto import UserIntentDTO, BidDTO
 
-def ingest_pipeline(user_intent_json, bid_json):
+def ingest_pipeline(user_intent: UserIntentDTO, bids: list[BidDTO]):
     db = Neo4jClient("bolt://localhost:7687", "neo4j", "testtest")
     builder = GraphBuilder(db)
 
-    print("Ingesting user intent and bids...", user_intent_json, bid_json)
+    print("Ingesting user intent and bids...", user_intent, bids)
+    builder.insert_user_intent(user_intent)
 
-    intent = UserIntentDTO(
-        intentHash=user_intent_json["intentHash"],
-        intentStatus=user_intent_json["intentStatus"],
-        account=user_intent_json["account"],
-        permittedAccounts=user_intent_json["userIntent"]["core"]["permittedAccounts"],
-        desiredAssets=user_intent_json["userIntent"]["constraints"]["desiredAssets"],
-        dispensableAssets=user_intent_json["userIntent"]["constraints"]["dispensableAssets"],
-        slippage=user_intent_json["userIntent"]["constraints"]["slippagePercentage"],
-        deadline=user_intent_json["userIntent"]["constraints"]["deadline"],
-        createdAt=user_intent_json["createdAt"]
-    )
-    builder.insert_user_intent(intent)
-
-    for b in bid_json:
-        bid = BidDTO(
-            bidHash=b["bidHash"],
-            intentHash=b["intentHash"],
-            solverAddress=b["solverAddress"],
-            smartWalletAddress=b["smartWalletAddress"],
-            status=b["bidStatus"],
-            steps=b["bid"]["steps"],
-            createdAt=b["createdAt"]
-        )
+    for bid in bids:
         builder.insert_bid(bid)
 
     db.close()
